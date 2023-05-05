f959fbd1195e877a612fe5a94d473698b510c604
weathermandgtl <120067549+weathermandgtl@users.noreply.github.com>
2023-05-05 14:48:19 -0400

    select from grid

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import mysql.connector
 import time
 import os
 
-# db = mysql.connector.connect(host="localhost", user="root", password="skilover1!", database="testdatabase")
-db = mysql.connector.connect(
-    host=os.getenv('MYSQLHOST'),
-    user=os.getenv('MYSQLUSER'),
-    port=os.getenv('MYSQLPORT'),
-    password=os.getenv('MYSQLPASSWORD'),
-    database=os.getenv('MYSQLDATABASE'))
+db = mysql.connector.connect(host="localhost", user="root", password="skilover1!", database="testdatabase")
+# db = mysql.connector.connect(
+#     host=os.getenv('MYSQLHOST'),
+#     user=os.getenv('MYSQLUSER'),
+#     port=os.getenv('MYSQLPORT'),
+#     password=os.getenv('MYSQLPASSWORD'),
+#     database=os.getenv('MYSQLDATABASE'))
 
 
 cursor = db.cursor()
 
-cursor.execute('SELECT * FROM grid WHERE id=1')
+cursor.execute('SELECT * FROM GridTest WHERE id=1')
 record = cursor.fetchone()
 print(record)
 
