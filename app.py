4759e4753fd0e161dbea2e48b94735e1d262c510
weathermandgtl <120067549+weathermandgtl@users.noreply.github.com>
2023-05-05 21:06:37 -0400

    add host port

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import mysql.connector
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-import os
 
-# db = mysql.connector.connect(host="localhost", user="root", password="skilover1!", database="testdatabase")
-db = mysql.connector.connect(
-    host=os.getenv('MYSQLHOST'),
-    user=os.getenv('MYSQLUSER'),
-    port=os.getenv('MYSQLPORT'),
-    password=os.getenv('MYSQLPASSWORD'),
-    database=os.getenv('MYSQLDATABASE'))
+db = mysql.connector.connect(host="localhost", user="root", password="skilover1!", database="testdatabase")
+# db = mysql.connector.connect(
+#     host=os.getenv('MYSQLHOST'),
+#     user=os.getenv('MYSQLUSER'),
+#     port=os.getenv('MYSQLPORT'),
+#     password=os.getenv('MYSQLPASSWORD'),
+#     database=os.getenv('MYSQLDATABASE'))
 
 app = Flask("Test")
 api = Api(app)
 parser = reqparse.RequestParser()
-parser.add_argument('title', required=True, location='form')
+# parser.add_argument('title', required=True, location='form')
 
 class Test(Resource):
     def get(self):
@@ -29,7 +28,7 @@ class Test(Resource):
 api.add_resource(Test, '/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
 
 
 
